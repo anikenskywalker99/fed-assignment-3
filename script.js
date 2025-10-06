17db28031f3d2b476cb9fb839243e084c554c83e
awiensshepherd <awiensshepherd@academic.rrc.ca>
2025-10-05 20:43:19 -0500

    Added Event Handling and State Management to the implementation of JavaScript.

--- script.js
+++ script.js
@@ -8,20 +8,39 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
     // Initial Setup
     let currentState = "start";
+    const answers = document.body.getElementById("answers")
+    const question = document.getElementById("question")
 
+    // Render Question
     function renderQuestion(currentState) {
         let text = "placeholder";
-        return `${text}`;
+        question.setAttribute("Your Adventure Begins now...", text);
+        while (answers.firstChild) {
+            answers.removeChild(answers.lastChild);
+        }
+        addAnswerButton(currentState)
     }
 
-    // Building the User Interface
+    // Add Answer Button
     function addAnswerButton(currentState) {
         let buttonOne = "First Placeholder";
         let newFirstButton = document.createElement("button");
         newFirstButton.textContent = buttonOne;
-        
+        answers.appendChild(newFirstButton);
+
         let buttonTwo = "Second Placeholder";
         let newSecondButton = document.createElement("button");
         newSecondButton.textContent = buttonTwo;
+        answers.appendChild(newSecondButton);
     }
+
+    newFirstButton.addEventListener("click", () => {
+        currentState = "Placeholder";
+        renderQuestion(currentState);
+    })
+    newSecondButton.addEventListener("click", () => {
+        currentState = "Placeholder";
+        renderQuestion(currentState);
+    })
+
 })
