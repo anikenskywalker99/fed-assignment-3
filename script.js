29171513c93a1bf8a2ae46a1dda654bf1ed96574
awiensshepherd <awiensshepherd@academic.rrc.ca>
2025-10-05 19:50:01 -0500

    initial Setup for Implementing JavaScript.

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+    Name: Aniken Wiens-Shepherd
+    Date: October 5, 2025
+    About: Creating an Adventure Game
+*/
+
+// Initial Setup 
+
+let currentState = "start";
+
+function renderQuestion(currentState) {
+    return `[Story Text Placeholder]`
+}
