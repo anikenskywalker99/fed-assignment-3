31b1ec03e677c90518afd1d1cdc768fc4bde5e89
awiensshepherd <awiensshepherd@academic.rrc.ca>
2025-10-05 20:10:07 -0500

    Built user interface for implementing JavaScript.

--- script.js
+++ script.js
@@ -4,10 +4,24 @@
     About: Creating an Adventure Game
 */
 
-// Initial Setup 
+document.addEventListener("DOMContentLoaded", (event) => {
 
-let currentState = "start";
+    // Initial Setup
+    let currentState = "start";
 
-function renderQuestion(currentState) {
-    return `[Story Text Placeholder]`
-}
+    function renderQuestion(currentState) {
+        let text = "placeholder";
+        return `${text}`;
+    }
+
+    // Building the User Interface
+    function addAnswerButton(currentState) {
+        let buttonOne = "First Placeholder";
+        let newFirstButton = document.createElement("button");
+        newFirstButton.textContent = buttonOne;
+        
+        let buttonTwo = "Second Placeholder";
+        let newSecondButton = document.createElement("button");
+        newSecondButton.textContent = buttonTwo;
+    }
+})
